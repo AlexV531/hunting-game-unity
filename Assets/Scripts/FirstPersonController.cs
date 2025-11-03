7c3862f823887e92e7ce563c9e929a087005a2c1
AlexV531 <v.alexander.531@gmail.com>
2025-11-03 17:59:53 -0500

    fixed inventory capacity

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -244,7 +244,7 @@ public class FirstPersonController : NetworkBehaviour
 		_inspectUI.SetPlayerInput(_input);
 		inventory.SetWeaponManager(_weaponManager);
 		inventory.SetCanHoldLargeItems(false);
-		inventory.SetCapacity(3);
+		// inventory.SetCapacity(3);
 		_playerInventoryMenu.SetPlayerInput(_input);
 #if ENABLE_INPUT_SYSTEM
 		_playerInput = GetComponent<PlayerInput>();
